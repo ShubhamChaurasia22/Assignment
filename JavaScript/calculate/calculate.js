6ac0e29c40d693fff4d38c52a580c5ff95d71e1d
ShubhamChaurasia22 <87766168+ShubhamChaurasia22@users.noreply.github.com>
2022-03-04 09:54:21 +0530

    first update

--- JavaScript/calculate/calculate.js
+++ JavaScript/calculate/calculate.js
@@ -1,42 +1,56 @@
-let array = [12,'+',3,'-',3,'**',2];
-let result = '';
-
-for(let i = 0; i < array.length; i++) {
-    switch(array[i]) {
-        case '+':
-            result = Number(array[i-1])  + Number(array[i+1]);
-            array[i+1] = result;
-            break;
-        case '-':
-            result = Number(array[i-1])  - Number(array[i+1]);
-            array[i+1] = result;
-            break;
-        case '*':
-            result = Number(array[i-1])  + Number(array[i+1]);
-            array[i+1] = result;
-            break;
-        case '/':
-            if(Number(array[i+1]) == 0) {
-                result = "Denominator can not be 0";
-            }else {
-                result = Number(array[i-1]) / Number(array[i+1]);
-                array[i+1] = result;
+function calculate(array) {
+    let result = '';
+    array.push();
+    console.log("array value is: ",array);
+    if(array.length === 0) {
+        console.log("Store some value");
+    }else {
+        for (let i = 0; i < array.length; i++) {
+            if ((i%2 === 0) && (array[i] === '+' || array[i] === '-' || array[i] === '*' || array[i] === '/' || array[i] === '%' || array[i]=== '**')) {
+                console.log("Operator must be in an odd place",array[i]);
             }
-            break;
-        case '%':
-            if(Number(array[i+1]) == 0) {
-                result = "Denominator can not be 0";
-            }else {
-                result = Number(array[i-1]) % Number(array[i+1]);
-                array[i+1] = result;
+            else if ((i%2 === 1) && !(isNaN(array[i]))) {
+                console.log("Operand must be in an even place",array[i]);
             }
-            break;
-        case '**':
-            result = Number(array[i-1])  ** Number(array[i+1]);
-            array[i+1] = result;
-            break;
-        default:
-            break;
+            else {
+                switch (array[i]) {
+                    case '+':
+                        result = (Number(array[i-1]) + Number(array[i+1])).toFixed(3);
+                        array[i+1] = result;
+                        break;
+                    case '-':
+                        result = (Number(array[i-1]) - Number(array[i+1])).toFixed(3);
+                        array[i+1] = result;
+                        break;
+                    case '*':
+                        result = (Number(array[i-1]) * Number(array[i+1])).toFixed(3);
+                        array[i+1] = result;
+                        break;
+                    case '/':
+                        if(array[i+1] === 0) {
+                            result = "Denominator can not be 0";
+                        }else {
+                            result = (Number(array[i-1]) / Number(array[i+1])).toFixed(3);
+                            array[i+1] = result;
+                        }
+                        break;
+                    case '%':
+                        if(array[i+1] === 0) {
+                            result = "Denominator can not be 0";
+                        }else {
+                            result = (Number(array[i-1]) % Number(array[i+1])).toFixed(3);
+                            array[i+1] = result;
+                        }
+                        break;
+                    case '**':
+                        result = (Number(array[i-1]) ** Number(array[i+1])).toFixed(3);
+                        array[i+1] = result;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
     }
+    return result;
 }
-console.log(result);
